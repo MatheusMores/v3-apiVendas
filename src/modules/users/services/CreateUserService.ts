93a1b118f6bf99a25e6020cf8f823c3a45afeade
MatheusMores <101605650+MatheusMores@users.noreply.github.com>
2023-02-07 13:00:41 -0300

    session jwebtoken

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import AppError from "@shared/errors/AppError";
+import { hash } from "bcryptjs";
 import { getCustomRepository } from "typeorm";
 import User from "../typeorm/entities/User";
 import UsersRepository from "../typeorm/repositories/UserRepository";
@@ -18,7 +19,9 @@ class CreateUserService {
             throw new AppError("Email address already used")
         }
 
-        const user = usersRepository.create({name, email, password});
+        const hashedPassword = await hash(password, 8);
+
+        const user = usersRepository.create({name, email, password: hashedPassword});
 
         await usersRepository.save(user);
         return user;
