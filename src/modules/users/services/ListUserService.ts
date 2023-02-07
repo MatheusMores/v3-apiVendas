63f9a601bc267ebc51683652976e17a5af6acc4d
MatheusMores <101605650+MatheusMores@users.noreply.github.com>
2023-02-07 12:30:15 -0300

    users

---
+++ src/modules/users/services/ListUserService.ts
@@ -0,0 +1,14 @@
+import { getCustomRepository } from "typeorm";
+import User from "../typeorm/entities/User";
+import UsersRepository from "../typeorm/repositories/UserRepository";
+
+class ListUserService {
+    async execute(): Promise<Array<User>>{
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        const users = await usersRepository.find();
+        return users;
+    }
+}
+
+export default ListUserService;
