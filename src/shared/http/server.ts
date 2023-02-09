42244a0604906720e9b7bc95762174c34014f5a7
MatheusMores <101605650+MatheusMores@users.noreply.github.com>
2023-02-09 11:06:12 -0300

    a

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -7,12 +7,13 @@ import "@shared/typeorm";
 
 import routes from './routes/index';
 import AppError from '@shared/errors/AppError';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 app.use(errors());
 
